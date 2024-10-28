187dbc0b6e7698adc6bbe011a6aa7fa332a4979c
Dnyaneshwar Mirge <dnyaneshwarmirge20@gmail.com>
2024-10-28 11:31:09 +0530

    q

--- ec2.tf
+++ ec2.tf
@@ -3,9 +3,9 @@ resource "aws_instance" "this_ubuntu" {
     ami = "ami-0dee22c13ea7a9a67" 
     disable_api_stop  = true 
     disable_api_termination = true  
-    instance_type = "t2.micro" 
+    instance_type = "t2.medium" 
     key_name = "dp"
-    count = 4  #loop 
+    count = 6  #loop 
     tags = {
       purpose = "jenkins server"
     } 

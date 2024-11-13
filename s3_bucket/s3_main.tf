4a22ad287d73a228c90f263d265554b4e1c3c667
Dnyaneshwar Mirge <dnyaneshwarmirge20@gmail.com>
2024-11-13 12:33:58 +0530

    Add files via upload

---
+++ s3_bucket/s3_main.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "mytfmainbucketgloble5525"
+
+  tags = {
+    Name = "My bucket"
+  }
+}
+ 
+resource "aws_s3_bucket_object" "my_object" {
+  bucket = aws_s3_bucket.my_bucket.bucket                   # Reference the bucket created above
+  key    = "ratnadeepsali05/terraform/s3_bucket/object"     # Path and name for the object in the bucket
+  source = "ratnadeepsali05/terraform/s3_bucket/object"      # Local path to the file you want to upload
+  acl    = "public-read"                                     # Optional: Set ACL, e.g., public-read for public access
+}

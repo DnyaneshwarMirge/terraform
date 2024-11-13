4b486f949e9673521a9ec35f0b342456568babef
Dnyaneshwar Mirge <dnyaneshwarmirge20@gmail.com>
2024-11-13 12:35:52 +0530

    Delete profile.tf

--- profile.tf
+++
@@ -1,6 +0,0 @@
-# profile.tf
-provider "aws" {
-  alias   = "test"
-  region  = "ap-south-1"              # Replace with your desired region
-  profile = "configs"   # Replace with the AWS CLI profile name you want to use
-}

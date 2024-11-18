69dbb874647123e3dc5b7066274d02a61bc048e5
Dnyaneshwar Mirge <dnyaneshwarmirge20@gmail.com>
2024-11-18 12:33:28 +0530

    q

--- module/dev/module.tf
+++ module/dev/module.tf
@@ -1,5 +1,5 @@
 module "ec2"{
-    source = ""
+    source = "/home/cloudshell-user/terraform/module/resources/ec2"
     this_image_id = "ami-09b0a86a2c84101e1"
     this_disable_api_stop = false
     this_disable_api_termination = false
@@ -8,7 +8,7 @@ module "ec2"{
 }
 
 module "aws_vpc" {
-        source = "/home/anup/deleteafterpractice/Terraform/Module/resources/VPC"
+        source = "/home/cloudshell-user/terraform/module/resources/vpc"
         this_vpc_cidr_block = "192.168.0.0/16"
         this_vpc_cidr_tags = "vpc"
         this_public_cidr_block  = "192.168.0.0/17"

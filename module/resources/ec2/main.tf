3b62553f89e72f973eaff6e55e894a980082e3bf
Dnyaneshwar Mirge <dnyaneshwarmirge20@gmail.com>
2024-11-18 12:36:03 +0530

    q

--- module/resources/ec2/main.tf
+++ module/resources/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "this_ubuntu" {
     ami = var.this_image_id 
-    disable_api_stop  = this_disable_api_stop
+    disable_api_stop  = var.this_disable_api_stop
     disable_api_termination = var.this_disable_api_termination
     instance_type = var.instance_type
     key_name= var.key_name

0a9243cdc76d2f7c6e1b13a60da4fe48bd1ec580
Dnyaneshwar Mirge <dnyaneshwarmirge20@gmail.com>
2024-11-19 11:04:01 +0530

    q

--- moduleblock/dev/module.tf
+++ moduleblock/dev/module.tf
@@ -1,17 +1,20 @@
-module "ec2"{
-    source = "/home/cloudshell-user/terraform/module/resources/ec2"
-    this_image_id = "ami-09b0a86a2c84101e1"
+module "ec2" {
+    source = "/home/anup/terraform/deleteafterpractice/Module/Resources/EC2"
+    this_image_id = "ami-012967cc5a8c9f891"
+    this_list = "t2.micro"
     this_disable_api_stop = false
     this_disable_api_termination = false
-    instance_type = "t2.micro"
-    key_name = "d"
+    #this_vpc_security_group_ids = "sg-0505874879e7ce6cd"
+    this_aws_instance_subnet = module.vpc.subnet_id
+    #this_sg_vpc_id = module.vpc.vpc_id
+    //this_aws_vpc_id = module.aws_vpc.aws_vpc_id
 }
 
-module "aws_vpc" {
-        source = "/home/cloudshell-user/terraform/module/resources/vpc"
-        this_vpc_cidr_block = "192.168.0.0/16"
-        this_vpc_cidr_tags = "vpc"
-        this_public_cidr_block  = "192.168.0.0/17"
-        this_public_map_pub = true
-        this_public_tags = "public"
-}
+module "vpc" {
+     source = "/home/anup/terraform/deleteafterpractice/Module/Resources/VPC"
+     this_vpc_cidr_block = "12.11.0.0/16"
+     this_vpc_tags = "this_vpc"
+     this_subnet_pub_cidr_block = "12.11.0.0/17"
+     this_subnet_pub_map_ip  = true 
+     this_subnet_pub_tags = "pub_subnet"
+     this_vpc_az = "us-east-1a"

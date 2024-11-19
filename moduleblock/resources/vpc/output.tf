43b3d7ad8d5d6ff32c8c94627aa1ea5f8eb1a98e
Dnyaneshwar Mirge <dnyaneshwarmirge20@gmail.com>
2024-11-19 11:55:48 +0530

    q

--- moduleblock/resources/vpc/output.tf
+++ moduleblock/resources/vpc/output.tf
@@ -1,6 +1,9 @@
-output "aws_vpc_id" {
-  value = aws_vpc.this_vpc.id
+output "vpc_id" {
+    value = aws_vpc.this_vpc.id
+  
 }
-output "aws_subnet_this_public" {
-  value = module.vpc.this_public.id
+
+output "subnet_id" {
+    value = aws_subnet.this_subnet_pub.id
+  
 }

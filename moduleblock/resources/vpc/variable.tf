088d0e0a8b99602097d6d49ebb4baf823bb79d67
Dnyaneshwar Mirge <dnyaneshwarmirge20@gmail.com>
2024-11-21 11:33:48 +0530

    q

--- moduleblock/resources/vpc/variable.tf
+++ moduleblock/resources/vpc/variable.tf
@@ -11,9 +11,9 @@ variable "this_subnet_pub_cidr_block" {
 variable "this_subnet_pub_map_ip" {
     type = bool
 }
-variable "this_subnet_pub_tags" {
-    type = string
-}
+#variable "this_subnet_pub_tags" {
+    #type = string
+#}
 
 variable "this_vpc_az" {
   type = string

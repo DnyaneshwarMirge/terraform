66939fcccfb72cd1fc1dff65f52fafc7569270ff
Dnyaneshwar Mirge <dnyaneshwarmirge20@gmail.com>
2024-11-21 11:40:18 +0530

    q

--- moduleblock/resources/ec2/main.tf
+++ moduleblock/resources/ec2/main.tf
@@ -1,33 +1,53 @@
-# Security Group to allow HTTP access
-resource "aws_security_group" "sg" {
-  #name        = "sg"
-  #description = "Allow HTTP traffic for Static app"
-  vpc_id      = var.vpc_id
-
+resource "aws_security_group" "web_server" {
+  #name        = var.ec2_security_group_name
+  #description = var.ec2_security_group_description
+  vpc_id      = var.ec2_vpc_id
+ 
   ingress {
-    description = "Allow HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "Allow traffic on port 80 from everywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
-
+   ingress {
+    description      = "Allow traffic on port 80 from everywhere"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+    ingress {
+    description      = "Allow traffic on port 80 from everywhere"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+ 
+  tags = {
+    Name = "sg"
   }
 }
 
 
-
-
-resource "aws_instance" "this_ubuntu" {
-    ami = var.this_image_id    #var.this_image_id 
+resource "aws_instance" "this_ec2" {
+    ami = var.this_This_ami    #var.this_image_id 
     disable_api_stop  = var.this_disable_api_stop  #var.this_disable_api_stop 
     disable_api_termination =  var.this_disable_api_termination  #var.this_disable_api_termination  
-    instance_type = var.instance_type  #variable "instance_type"
+    instance_type = var.this_ec2type  #var.this_any.instance_type_list[0]
     subnet_id =  var.this_aws_instance_subnet 
-    security_groups = [aws_security_group.sg.name]
+    key_name                = var.This_key
+    vpc_security_group_ids  = [aws_security_group.web_server.id]
+
 }   
--- moduleblock/resources/ec2/variable.tf
+++ moduleblock/resources/ec2/variable.tf
@@ -1,25 +1,43 @@
+variable "This_key" {
+    type = string   
+}
+
+
+variable "This_ami" {
+     type = string    
+}
+variable "this_aws_vpc_id" {
+    type = string
+}
+
+variable "ec2_vpc_id" {
+  type = string
+} 
+
+
 variable "this_image_id" {
   type        = string
 }
 
+
+
+
+variable "this_ec2type" {
+    type = string
+     
+}
+
 variable "this_disable_api_stop" {
-  type        = bool
+     type = bool
+
 }
 
 variable "this_disable_api_termination" {
-    type = bool 
+  type = bool
+
 }
 
-variable "instance_type" {
-  type        = string
-}
-
-variable "key_name" {
-  type        = string
-}
 variable "this_aws_instance_subnet" {
    type = string
-}
-variable "vpc_id" {
-  type        = string
+
 }

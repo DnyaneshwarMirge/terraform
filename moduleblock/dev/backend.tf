e58d5baeff0c7cb296b4c6921040697484d64e20
Dnyaneshwar Mirge <dnyaneshwarmirge20@gmail.com>
2024-11-21 11:27:15 +0530

    q

--- moduleblock/dev/backend.tf
+++ moduleblock/dev/backend.tf
@@ -6,6 +6,6 @@ terraform {
         dynamodb_table = "cbz38"
         region = "ap-south-1"
         profile = "configs"
-        shared_credentials_files = "/home/cloudshell-user/.aws/credentials"
+        shared_credentials_files = ["/home/cloudshell-user/.aws/credentials"]
     }
 }

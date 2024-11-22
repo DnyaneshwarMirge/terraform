1113ff3e64c0ddce423f19e5ff3a206a7f0548e4
Dnyaneshwar Mirge <dnyaneshwarmirge20@gmail.com>
2024-11-22 11:24:17 +0530

    q

--- loops/countloop.tf
+++ loops/countloop.tf
@@ -1,11 +1,15 @@
+# Resource to create buckets based on the `users_list` variable
 resource "aws_s3_bucket" "my_bucket" { 
-    count = length(var.users_list)  #3
-    bucket = var.users_list[count.index]
-}  
+    count  = length(var.users_list)
+    bucket = "my-unique-${var.users_list[count.index]}"  # Add a unique prefix
+}
+
+# Resource to create 3 buckets with valid names
 resource "aws_s3_bucket" "bucket38" { 
-    bucket = "D.${count.index}"   #neo
-    count = 3 
-}  
+    bucket = "my-unique-d-${count.index}"  # Valid lowercase bucket names
+    count  = 3
+}
+
 
 
 variable "users_list" {

ca22345366cd15f5b3bfb29b0c4a9f75def74a68
Dnyaneshwar Mirge <dnyaneshwarmirge20@gmail.com>
2024-11-22 11:11:08 +0530

    q

--- loops/countloop.tf
+++ loops/countloop.tf
@@ -1,14 +1,14 @@
-resource "aws_iam_user" "this_aws_iam_user" { 
+resource "aws_s3_bucket" "my_bucket" { 
     count = length(var.users_list)  #3
     name = var.users_list[count.index]
 }  
-resource "aws_iam_user" "this_aws_iam_user_2" { 
+resource "aws_s3_bucket" "my_bucket" { 
     name = "D.${count.index}"   #neo
     count = 3 
 }  
 
 
 variable "users_list" {
-    default = ["user1" , "user2" , "user3"]
+    default = ["bucket-1" , "bucket-2" , "bucket-3"]
 }                #0         #1       #2
   

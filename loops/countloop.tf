92ead7f2aab5481fb5dd77fccec1cf15406782c1
Dnyaneshwar Mirge <dnyaneshwarmirge20@gmail.com>
2024-11-22 11:33:49 +0530

    q

--- loops/countloop.tf
+++ loops/countloop.tf
@@ -1,21 +1,14 @@
-# Resource to create buckets based on the `users_list` variable
-resource "aws_s3_bucket" "my_bucket" { 
-    count  = length(var.users_list)
-    bucket = "my-unique-${var.users_list[count.index]}"
-    force_destroy = true  # Deletes the bucket even if it contains objects
-}
-
-
-# Resource to create 3 buckets with valid names
-resource "aws_s3_bucket" "bucket38" { 
-    bucket = "my-unique-d-${count.index}"  # Valid lowercase bucket names
-    force_destroy = true  # Deletes the bucket even if it contains objects
-    count  = 3
-}
-
+resource "aws_iam_user" "person-1" { 
+    count = length(var.users_list)  #3
+    name = var.users_list[count.index]
+}  
+resource "aws_iam_user" "person-2" { 
+    name = "D.${count.index}"   #neo
+    count = 3 
+}  
 
 
 variable "users_list" {
-    default = ["bucket-1" , "bucket-2" , "bucket-3"]
+    default = ["user1" , "user2" , "user3"]
 }                #0         #1       #2
   

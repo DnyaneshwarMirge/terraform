e34ca285c455335891a6d52e9da2e84685014e03
Dnyaneshwar Mirge <dnyaneshwarmirge20@gmail.com>
2024-11-23 12:29:11 +0530

    qq

---
+++ provisioners/provisionersfile/provisionersfile.tf
@@ -0,0 +1,21 @@
+ resource "aws_instance" "this_aws_instance" {
+    ami = "ami-0614680123427b75e"
+    vpc_security_group_ids = ["sg-0f4734f182e556bae"]
+    key_name = "anup_delete"
+    instance_type = "t2.micro"
+     
+     provisioner "file" {
+    source      = "readme.md"
+    destination = "/home/ec2-user/readme.md"
+      connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("${path.module}/id_rsa")
+    host     = "${self.public_ip}"
+  }
+  
+  
+  }
+
+
+}

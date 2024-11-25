4e22ba2d5f68676bc20b191b3007b68e4da78db6
Dnyaneshwar Mirge <dnyaneshwarmirge20@gmail.com>
2024-11-25 10:44:36 +0530

    q

--- provisioners/remoteprovisioners/remoteprovisioners.tf
+++ provisioners/remoteprovisioners/remoteprovisioners.tf
@@ -1,31 +1,43 @@
 provider "aws" {
     region =  "ap-south-1"
     profile = "configs"
-} 
- 
- resource "aws_instance" "this_aws_instance" {
-    ami = "ami-0614680123427b75e"
-    vpc_security_group_ids = ["sg-0f4734f182e556bae"]
-    key_name = "d"
-    instance_type = "t2.micro"
-     
-     provisioner "file" {
-    source      = "readme.md"
-    destination = "/home/ec2-user/readme.md"
-      connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    private_key = file("${path.module}/id_rsa.pem")
-    host     = "${self.public_ip}"
-  } 
-  }
- provisioner "remote-exec" {
+}
+
+resource "aws_instance" "this_aws_instance" {
+  ami = "ami-0614680123427b75e"
+  vpc_security_group_ids = ["sg-0f4734f182e556bae"]
+  key_name = "d"
+  instance_type = "t2.micro"
+
+  provisioner "remote-exec" {
     inline = [
-      "ifconfig > /tmp/ifconfig.out",
-      "echo 'hello world' > /tmp/test.txt",
+      "sudo yum update",
+      "sudo yum install -y nginx",
+      "sudo systemctl start nginx"
     ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user" # Default user for ec2-user AMIs; replace if needed
+      private_key = file("${path.module}/id_rsa.pem")
+      host        = self.public_ip
+    }
   }
 
+  tags = {
+    Name = "MyInstance"
+  }
+}
 
- }
+/*provisioner "remote-exec" {
+  script = "path/to/script.sh"
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("path/to/id_rsa.pem")
+    host        = self.public_ip
+  }
+}*/
+
  
